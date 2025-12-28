0f13a722f9a7a10b09c83640bbc67ebcc8c422e7
6pasttime <isikopeter@6pasttime.com>
2025-12-28 04:50:11 +0300

    switch to oauth 1.0 + fix redirect uri

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ export const authOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-      version: "2.0",
+      // Remove version: "2.0" to use stable OAuth 1.0
       authorization: { params: { scope: "tweet.read users.read offline.access" } },
     }),
   ],
